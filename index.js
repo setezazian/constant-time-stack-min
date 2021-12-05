d2a7291373fbb6751e8d1b8a5bf3b31ebd9e92ab
Shirin Etezazian <64745415+setezazian@users.noreply.github.com>
2021-12-05 01:13:12 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+ // Stack Class
+  var Stack = function() {
+
+  // add an item to the top of the stack
+    this.push = function(value) {
+    };
+
+  // remove an item from the top of the stack
+    this.pop = function() {
+    };
+
+  // return the number of items in the stack
+    this.size = function() {
+    };
+
+  // return the minimum value in the stack
+    this.min = function() {
+
+    };
+
+  };
